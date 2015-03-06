12cabd2f1c7e94d7e986f742d6b23a60c6295b4b
magixsource <magixsource.cn@gmail.com>
2015-03-06 14:22:36 +0800

    [Mod] Refactory code,simple pagination *2

--- src/com/linpeng/advisor/controller/IngredientsController.java
+++ src/com/linpeng/advisor/controller/IngredientsController.java
@@ -1,7 +1,6 @@
 package com.linpeng.advisor.controller;
 
 import com.jfinal.core.Controller;
-import com.jfinal.plugin.activerecord.Page;
 import com.linpeng.advisor.model.Ingredient;
 
 /**
@@ -16,17 +15,11 @@ public class IngredientsController extends Controller {
 	 * pagination ingredients
 	 */
 	public void list() {
-		int pageNumber = getParaToInt("page", 1);
+		int pageNumber = getParaToInt(0, 1);
 		int pageSize = getParaToInt("pagesize", 10);
 
-		Page<Ingredient> page = Ingredient.dao.paginate(pageNumber, pageSize,
-				"select *", " from Ingredients");
-
-		setAttr("ingredients", page.getList());
-		setAttr("page", page.getPageNumber());
-		setAttr("pagesize", page.getPageSize());
-		setAttr("total", page.getTotalRow());
-		setAttr("totalpage", page.getTotalPage());
+		setAttr("page", Ingredient.dao.paginate(pageNumber, pageSize,
+				"select *", " from Ingredients"));
 
 		render("list.html");
 	}
