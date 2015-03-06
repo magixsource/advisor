c413961af108b5f28ef01a8d5390a0336b840b31
magixsource <magixsource.cn@gmail.com>
2015-03-06 14:20:25 +0800

    [Add]

---
+++ src/com/linpeng/advisor/controller/DashboardController.java
@@ -0,0 +1,13 @@
+package com.linpeng.advisor.controller;
+
+import com.jfinal.core.Controller;
+
+/**
+ * Dashboard Controller
+ * 
+ * @author linpeng
+ *
+ */
+public class DashboardController extends Controller {
+
+}
