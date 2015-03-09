acc313d3113a817a828b824f6148bbe4c6eceaa7
magixsource <magixsource.cn@gmail.com>
2015-03-09 17:23:31 +0800

    [Add] Add menu inject intercepter to application

--- src/com/linpeng/advisor/config/BaseConfig.java
+++ src/com/linpeng/advisor/config/BaseConfig.java
@@ -17,11 +17,14 @@ import com.linpeng.advisor.controller.DictionaryController;
 import com.linpeng.advisor.controller.DiseaseController;
 import com.linpeng.advisor.controller.IngredientsController;
 import com.linpeng.advisor.controller.LoginController;
+import com.linpeng.advisor.controller.MenuController;
 import com.linpeng.advisor.controller.SignController;
+import com.linpeng.advisor.interceptor.MenuInjectIntercepter;
 import com.linpeng.advisor.model.Dictionary;
 import com.linpeng.advisor.model.DictionaryKind;
 import com.linpeng.advisor.model.Disease;
 import com.linpeng.advisor.model.Ingredient;
+import com.linpeng.advisor.model.Menu;
 import com.linpeng.advisor.model.User;
 
 /**
@@ -44,6 +47,7 @@ public class BaseConfig extends JFinalConfig {
 		// Common-Manage module
 		me.add("/login", LoginController.class, "login");
 		me.add("/signup", SignController.class, "signup");
+		me.add("/menu", MenuController.class, "menu");
 
 		// User Module
 
@@ -74,11 +78,13 @@ public class BaseConfig extends JFinalConfig {
 		arp.addMapping("disease", Disease.class);
 		arp.addMapping("dictionary_kind", DictionaryKind.class);
 		arp.addMapping("dictionary", Dictionary.class);
+		arp.addMapping("menu", Menu.class);
 	}
 
 	@Override
 	public void configInterceptor(Interceptors me) {
 		// me.add(new AuthInterceptor());
+		me.add(new MenuInjectIntercepter());
 	}
 
 	@Override
