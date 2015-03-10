5eda94b2265109098d3a5d36c1e452030a8908b7
magixsource <magixsource.cn@gmail.com>
2015-03-10 16:19:10 +0800

    [Add] Add StringUtils,support operation array2string and string2array

---
+++ src/com/linpeng/advisor/common/StringUtils.java
@@ -0,0 +1,66 @@
+package com.linpeng.advisor.common;
+
+/**
+ * String & Array tools
+ * 
+ * @author linpeng
+ *
+ */
+public class StringUtils {
+	/** Default Delimiter String */
+	public static final String DEFAULT_DELIMITER_STRING = ",";
+
+	/**
+	 * Java array to String by DEFAULT_DELIMITER_STRING
+	 * 
+	 * @param array
+	 *            source array
+	 * @return String
+	 */
+	public static String array2string(String[] array) {
+		return array2string(array, DEFAULT_DELIMITER_STRING);
+	}
+
+	/**
+	 * According delimiter parse array to string
+	 * 
+	 * @param array
+	 *            source array
+	 * @param delimiter
+	 *            according by
+	 * @return string
+	 */
+	public static String array2string(String[] array, String delimiter) {
+		StringBuilder sb = new StringBuilder();
+		for (String str : array) {
+			sb.append(str);
+			sb.append(delimiter);
+		}
+		return sb.substring(0, sb.length() - 1);
+	}
+
+	/**
+	 * String parse to Java Array
+	 * 
+	 * @param str
+	 *            a string should split by DEFAULT_DELIMITER_STRING
+	 * @return array
+	 */
+	public static String[] string2array(String str) {
+		return string2array(str, DEFAULT_DELIMITER_STRING);
+	}
+
+	/**
+	 * According delimiter parse String to Java Array
+	 * 
+	 * @param str
+	 *            a string should split by delimiter
+	 * @param delimiter
+	 *            string should split by
+	 * @return array
+	 */
+	public static String[] string2array(String str, String delimiter) {
+		return str.split(delimiter);
+	}
+
+}
