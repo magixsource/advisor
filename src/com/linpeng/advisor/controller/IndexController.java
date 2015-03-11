77e1afecc1a9651a6e68a3490c315072fd9a8448
magixsource <magixsource.cn@gmail.com>
2015-03-11 08:57:44 +0800

    [Add] Add Index controller & index.html

---
+++ src/com/linpeng/advisor/controller/IndexController.java
@@ -0,0 +1,16 @@
+package com.linpeng.advisor.controller;
+
+import com.jfinal.core.Controller;
+
+/**
+ * Application index controller
+ * 
+ * @author linpeng
+ *
+ */
+public class IndexController extends Controller {
+	
+	public void index() {
+
+	}
+}
