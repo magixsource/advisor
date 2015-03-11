19dda3c498816ace4cf26c4248907f1ab7f39231
magixsource <magixsource.cn@gmail.com>
2015-03-11 10:57:14 +0800

    [Mod] Authintercepter should be ignore by aopignore annotation

--- src/com/linpeng/advisor/interceptor/AuthInterceptor.java
+++ src/com/linpeng/advisor/interceptor/AuthInterceptor.java
@@ -3,7 +3,7 @@ package com.linpeng.advisor.interceptor;
 import com.jfinal.aop.Interceptor;
 import com.jfinal.core.ActionInvocation;
 import com.jfinal.core.Controller;
-import com.linpeng.advisor.controller.IndexController;
+import com.linpeng.advisor.annotation.AopIgnore;
 import com.linpeng.advisor.model.User;
 
 /**
@@ -14,14 +14,36 @@ import com.linpeng.advisor.model.User;
  */
 public class AuthInterceptor implements Interceptor {
 
+	public static final String UNIQUE_INTERCEPTOR_ID = "AUTHINTERCEPTER";
+
 	public void intercept(ActionInvocation ai) {
 		Controller controller = ai.getController();
 		User loginUser = controller.getSessionAttr("loginUser");
-		if (loginUser != null || (controller instanceof IndexController)) {
+		if (loginUser != null || isIgnoreMe(controller)) {
 			ai.invoke();
 		} else {
 			controller.redirect("/login/login");
 		}
+	}
+
+	/**
+	 * Check is this should ignore
+	 * 
+	 * @param controller
+	 * @return
+	 */
+	private boolean isIgnoreMe(Controller controller) {
+		AopIgnore annotation = controller.getClass().getAnnotation(
+				AopIgnore.class);
+		if (null == annotation || null == annotation.value()) {
+			return false;
+		}
+		for (String value : annotation.value()) {
+			if (value.equalsIgnoreCase(UNIQUE_INTERCEPTOR_ID)) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 }
